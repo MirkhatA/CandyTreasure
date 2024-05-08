82cc8b8e84095e910dc16a81e4a7d254137b5da3
Mirkek <57861727+MirkhatA@users.noreply.github.com>
2024-05-08 14:39:32 +0500

    facebook sdk

---
+++ Assets/Scripts/ADB/FacebookManager.cs
@@ -0,0 +1,69 @@
+using Facebook.Unity;
+using UnityEngine;
+using System;
+using UnityEngine.SceneManagement;
+
+public class FacebookManager : MonoBehaviour
+{
+    public string deepLink;
+    public BrowserOpener browserOpener;
+    public string starterUrl = "http://candytreasures.xyz?bonus1=";
+
+    void Start()
+    {
+        if (!FB.IsInitialized)
+        {
+            FB.Init(InitCallback, OnHideUnity);
+        }
+        else
+        {
+            FB.ActivateApp();
+        }
+    }
+
+    void DeepLinkCallback(IAppLinkResult linkResult)
+    {
+        if (!string.IsNullOrEmpty(linkResult.TargetUrl))
+        {
+            var res = linkResult.TargetUrl;
+            Console.WriteLine("res::" + res);
+            if (res != "null")
+            {
+                deepLink = $"54mGbLCw?{res}";
+                browserOpener.deeplink = deepLink;
+                browserOpener.OpenBrowserGame(starterUrl);
+            }
+        }
+        else
+        {
+            SceneManager.LoadScene(1);
+        }
+    }
+
+    private void InitCallback()
+    {
+        Console.WriteLine("InitCallback!");
+        if (FB.IsInitialized)
+        {
+            Console.WriteLine("IsInitialized!");
+            FB.ActivateApp();
+            FB.Mobile.FetchDeferredAppLinkData(DeepLinkCallback);
+        }
+        else
+        {
+            Console.WriteLine("Failed to Initialize the Facebook SDK");
+        }
+    }
+
+    private void OnHideUnity(bool isGameShown)
+    {
+        if (!isGameShown)
+        {
+            Time.timeScale = 0;
+        }
+        else
+        {
+            Time.timeScale = 1;
+        }
+    }
+}
---
+++ Assets/Scripts/ADB/webview/BrowserOpener.cs
@@ -0,0 +1,87 @@
+﻿using UnityEngine;
+using System;
+
+public class BrowserOpener : MonoBehaviour {
+
+	public string URL = "";
+	public string deeplink = "";
+
+    private string streamId = "";
+    private string offername = "";
+    private string sub1 = "";
+    private string sub2 = "";
+    private string sub3 = "";
+    private string sub4 = "";
+
+    public int startIdx;
+
+    private string userId = "";
+    private string bundle = "com.cand.ytreasu.res";
+
+    public void OpenBrowserGame(string url)
+    {
+        InAppBrowser.DisplayOptions options = new InAppBrowser.DisplayOptions();
+		options.displayURLAsPageTitle = false;
+		options.hidesTopBar = true;
+		options.hidesHistoryButtons = true; 
+		options.insets = new InAppBrowser.EdgeInsets { bottom = 0, left = 0 };
+
+        if (deeplink != "") {
+            FindStartIdx();
+            GenerateUserId();
+            ParseURL();
+            URL = url + "" + sub1 + "&bonus2=" + sub2 + "&bonus3=" + sub3 + "&bonus4=" + sub4 + "&bonusid=" + streamId + "&offerbonus=" + offername + "&uniqid=" + userId + "&bundle=" + bundle;
+        } else
+        {
+            URL = "https://candytreasures.xyz/";
+        }
+
+        Console.WriteLine("URL::" + URL);
+		InAppBrowser.SetUserAgent("Mozilla/5.0 (Linux; Android 10) AppleWebKit/537.36 (KHTML, like Gecko) Firefox/85.0 Mobile Safari/537.36");
+		InAppBrowser.OpenURL(URL, options);
+	}
+
+    public void FindStartIdx()
+    {
+        var id = deeplink.IndexOf("app://");
+        startIdx += id + 8;
+    }
+
+    public void ParseURL()
+	{
+        if (startIdx != 0)
+        {
+            string remainingString = deeplink.Substring(startIdx);
+            string[] elements = remainingString.Split('_');
+
+            if (elements.Length >= 6)
+            {
+                streamId = elements[2];
+                offername = elements[1];
+                sub1 = elements[3]; 
+                sub2 = elements[4];
+                sub3 = elements[5];
+                sub4 = elements[6];
+                Console.WriteLine("streamId::" + streamId);
+                Console.WriteLine("offername::" + offername);
+                Console.WriteLine("sub1::" + sub1);
+                Console.WriteLine("sub2::" + sub2);
+                Console.WriteLine("sub3::" + sub3);
+                Console.WriteLine("sub4::" + sub4);
+            }
+            else
+            {
+                Debug.LogError("Not enough elements in the URL.");
+            }
+        }
+        else
+        {
+            Debug.LogError("Start index not found.");
+        }
+    }
+
+    public void GenerateUserId()
+    {
+        userId = DateTime.Now.Ticks.ToString() + UnityEngine.Random.Range(0, 1000000).ToString();
+    }
+}
